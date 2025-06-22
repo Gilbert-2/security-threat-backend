29c9103be27a4e3f6f50ed4eef5860edea87e0ab
Gilbert-2 <gilbertrick81@gmail.com>
2025-06-22 19:04:46 +0200

    added database configuration

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,11 +1,14 @@
+import { url } from "inspector";
+
 export default () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
   database: {
-    host: process.env.DATABASE_HOST,
-    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    // host: process.env.DATABASE_HOST,
+    // port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+    // username: process.env.DATABASE_USERNAME,
+    // password: process.env.DATABASE_PASSWORD,
+    // database: process.env.DATABASE_NAME,
+    url: process.env.DATABASE_URL
   },
   jwt: {
     secret: process.env.JWT_SECRET,
